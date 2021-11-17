660b4c9671ba189d22ea0a6303e776594c5546e8
shlili <92100974+shlili@users.noreply.github.com>
2021-11-17 14:52:10 +0100

    Add files via upload

---
+++ travel.js
@@ -0,0 +1,205 @@
+var isLiked1=false;
+var a1=0;
+ function onClick1()
+  {
+  isLiked1=!isLiked1;
+  if(isLiked1) {
+    a1 = document.getElementById("totalLikes1").innerHTML = parseInt(a1)+1;
+  }
+  else {
+
+  a1 = document.getElementById("totalLikes1").innerHTML = parseInt(a1)-1;
+  }
+
+  }
+  var isLiked2=false;
+var a2=0;
+ function onClick2()
+  {
+  isLiked2=!isLiked2;
+  if(isLiked2) {
+    a2 = document.getElementById("totalLikes2").innerHTML = parseInt(a2)+1;
+  }
+  else {
+
+  a2 = document.getElementById("totalLikes2").innerHTML = parseInt(a2)-1;
+  }
+
+  }
+  var isLiked3=false;
+var a3=0;
+ function onClick3()
+  {
+  isLiked3=!isLiked3;
+  if(isLiked3) {
+    a3 = document.getElementById("totalLikes3").innerHTML = parseInt(a3)+1;
+  }
+  else {
+
+  a3 = document.getElementById("totalLikes3").innerHTML = parseInt(a3)-1;
+  }
+
+  }
+  var isLiked4=false;
+var a4=0;
+ function onClick4()
+  {
+  isLiked4=!isLiked4;
+  if(isLiked4) {
+    a4 = document.getElementById("totalLikes4").innerHTML = parseInt(a4)+1;
+  }
+  else {
+
+  a4 = document.getElementById("totalLikes4").innerHTML = parseInt(a4)-1;
+  }
+
+  }
+  var isLiked5=false;
+var a5=0;
+ function onClick5()
+  {
+  isLiked5=!isLiked5;
+  if(isLiked5) {
+    a5 = document.getElementById("totalLikes5").innerHTML = parseInt(a5)+1;
+  }
+  else {
+
+  a5 = document.getElementById("totalLikes5").innerHTML = parseInt(a5)-1;
+  }
+
+  }
+  var isLiked6=false;
+var a6=0;
+ function onClick6()
+  {
+  isLiked6=!isLiked6;
+  if(isLiked6) {
+    a6 = document.getElementById("totalLikes6").innerHTML = parseInt(a6)+1;
+  }
+  else {
+
+  a6 = document.getElementById("totalLikes6").innerHTML = parseInt(a6)-1;
+  }
+
+  }
+  var isLiked7=false;
+var a7=0;
+ function onClick7()
+  {
+  isLiked7=!isLiked7;
+  if(isLiked7) {
+    a7 = document.getElementById("totalLikes7").innerHTML = parseInt(a7)+1;
+  }
+  else {
+
+  a7 = document.getElementById("totalLikes7").innerHTML = parseInt(a7)-1;
+  }
+
+  }
+  var isLiked8=false;
+var a8=0;
+ function onClick8()
+  {
+  isLiked8=!isLiked8;
+  if(isLiked8) {
+    a8 = document.getElementById("totalLikes8").innerHTML = parseInt(a8)+1;
+  }
+  else {
+
+  a8 = document.getElementById("totalLikes8").innerHTML = parseInt(a8)-1;
+  }
+
+  }
+  var isLiked9=false;
+var a9=0;
+ function onClick9()
+  {
+  isLiked9=!isLiked9;
+  if(isLiked9) {
+    a9 = document.getElementById("totalLikes9").innerHTML = parseInt(a9)+1;
+  }
+  else {
+
+  a9 = document.getElementById("totalLikes9").innerHTML = parseInt(a9)-1;
+  }
+
+  }
+  var isLiked10=false;
+var a10=0;
+ function onClick10()
+  {
+  isLiked10=!isLiked10;
+  if(isLiked10) {
+    a10 = document.getElementById("totalLikes10").innerHTML = parseInt(a10)+1;
+  }
+  else {
+
+  a10 = document.getElementById("totalLikes10").innerHTML = parseInt(a10)-1;
+  }
+
+  }
+  var isLiked11=false;
+var a11=0;
+ function onClick11()
+  {
+  isLiked11=!isLiked11;
+  if(isLiked11) {
+    a11 = document.getElementById("totalLikes11").innerHTML = parseInt(a11)+1;
+  }
+  else {
+
+  a11 = document.getElementById("totalLikes11").innerHTML = parseInt(a11)-1;
+  }
+
+  }
+  var isLiked12=false;
+var a12=0;
+ function onClick12()
+  {
+  isLiked12=!isLiked12;
+  if(isLiked12) {
+    a12 = document.getElementById("totalLikes12").innerHTML = parseInt(a12)+1;
+  }
+  else {
+
+  a12 = document.getElementById("totalLikes12").innerHTML = parseInt(a12)-1;
+  }
+
+  }
+
+  document.querySelector('.like-button1').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked1');
+  });
+  document.querySelector('.like-button2').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked2');
+  });
+  document.querySelector('.like-button3').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked3');
+  });
+  document.querySelector('.like-button4').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked4');
+  });
+  document.querySelector('.like-button5').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked5');
+  });
+  document.querySelector('.like-button6').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked6');
+  });
+  document.querySelector('.like-button7').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked7');
+  });
+  document.querySelector('.like-button8').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked8');
+  });
+  document.querySelector('.like-button9').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked9');
+  });
+  document.querySelector('.like-button10').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked10');
+  });
+  document.querySelector('.like-button11').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked11');
+  });
+  document.querySelector('.like-button12').addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('liked12');
+  });
